fa9f89e31a976b07ef9a89cd507ac18b75e84fd5
zhuwendong <zhuwendong@sinohealth.com>
2021-05-19 10:24:03 +0800

    chore: test

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,14 +2,14 @@
  * @Author: atdow
  * @Date: 2020-10-21 11:18:41
  * @LastEditors: null
- * @LastEditTime: 2021-05-12 14:35:20
+ * @LastEditTime: 2021-05-18 17:49:26
  * @Description: file content
  */
 import { isIE } from '@src/utils/util.js';
 
 // console.log('process.env.NODE_ENV:', process.env.NODE_ENV); development
 // 判断环境不是 prod 或者 preview 是 true 时，加载 mock 服务
-if (process.env.NODE_ENV !== 'production' || process.env.VUE_APP_PREVIEW === true) {
+if (process.env.NODE_ENV !== 'production' || process.env.VUE_APP_PREVIEW === true || true) {
     //if (process.env.NODE_ENV !== 'production') {
     if (isIE()) {
         console.error(
